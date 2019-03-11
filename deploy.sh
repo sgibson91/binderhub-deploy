ac8d42f4d28f9b2779f735ac322488fc8fa89e58
Sarah Gibson <sgibson@turing.ac.uk>
2019-03-11 15:06:07 +0000

    Add deploy.sh script to configure and deploy a JupyterHub/BinderHub
    Add deploy.sh shell script which automatically installs, configures
    and deploys both a JupyterHub and BinderHub

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Required inputs:
+# * $id: DockerHub ID
+# * $prefix: desired image prefix
+# * $version: Helm Chart version to deploy
+# * $binderhubname: Name/Namespace for the BinderHub
+
+# Optional inputs:
+# * $org: DockerHub organisation
+# * $secretFile: Path to file containing secrets/passwords
+
+# Exit immediately if a pipeline returns a non-zero status
+set -e
+
+# Create tokens for the secrets file:
+apiToken=`openssl rand -hex 32`
+secretToken=`openssl rand -hex 32`
+
+# Get the latest helm chart for BinderHub:
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
+helm repo update
+
+# Install the Helm Chart using the configuration files, to deploy both a BinderHub and a JupyterHub:
+python create_config.py -id=$id --prefix=$prefix -org=$org --force
+python create_secret.py --apiToken=$apiToken --secretToken=$secretToken --secretFile=$secretFile --force
+helm install jupyterhub/binderhub --version=$version --name=$binderhubname --namespace=$binderhubname -f secret.yaml -f config.yaml
+
+# Wait for  JupyterHub, grab its IP address, and update BinderHub to link together:
+jupyterhub_ip=`kubectl --namespace=$binderhubname get svc proxy-public | awk '{ print $4}' | tail -n 1`
+while [ "$jupyterhub_ip" = '<pending>' ] || [ "$jupyterhub_ip" = "" ]
+do
+    echo "JupyterHub IP: $jupyterhub_ip"
+    sleep 5
+    jupyterhub_ip=`kubectl --namespace=$binderhubname get svc proxy-public | awk '{ print $4}' | tail -n 1`
+done
+python create-config.py --id=$id --prefix=$prefix -org=$org --jupyterhub_ip=$jupyterhub_ip --force
+helm upgrade $binderhubname jupyterhub/binderhub --version=$version -f secret.yaml -f config.yaml
