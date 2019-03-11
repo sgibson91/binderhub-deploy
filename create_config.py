299dbbf45e2987750f79e7e4da6a0c3f2b76151d
Sarah Gibson <sgibson@turing.ac.uk>
2019-03-11 14:15:38 +0000

    Add python script to automatically write a config.yaml file
    Python script automatically writes a config.yaml file to launch a
    BinderHub using a DockerHub image registry. Uses a template config
    file for layout.

---
+++ create_config.py
@@ -0,0 +1,61 @@
+import yaml
+import argparse
+import os
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-id", "--docker-id", type=str, required=True,
+                        help="Docker ID")
+    parser.add_argument("--prefix", type=str, required=True,
+                        help="Docker image prefix")
+    parser.add_argument("-org", "--docker-org", default=None,
+                        help="Docker organisation ID")
+    parser.add_argument("--jupyterhub_ip", type=str, default=None,
+                        help="IP address of the JupyterHub")
+    parser.add_argument("--template", type=str, default="config-template.yaml",
+                        help="Template config file")
+    parser.add_argument("--force", action="store_true",
+                        help="Overwrite existing files")
+    parser.add_argument("output_file", nargs="?", default="config.yaml",
+                        help="Output file for config")
+
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    if os.path.exists(args.output_file):
+        if args.force == True:
+            os.remove(args.output_file)
+        else:
+            raise RuntimeError("Output file already exists: {}".format(
+                args.output_file))
+
+    template = yaml.load(open(args.template, "r"))
+    if not (args.docker_org is None):
+        template['config']['BinderHub']['image_prefix'] = (
+            template['config']['BinderHub']['image_prefix'].replace(
+                "<docker-id>", "{0}|{1}".format(args.docker_id, args.docker_org)
+            ).replace("<prefix>", args.prefix)
+        )
+    else:
+        template['config']['BinderHub']['image_prefix'] = (
+            template['config']['BinderHub']['image_prefix'].replace(
+            "<docker-id>", args.docker_id).replace("<prefix>", args.prefix)
+        )
+
+    if not (args.jupyterhub_ip is None):
+        template['hub'] = {}
+        template['hub']['url'] = "http://{}".format(args.jupyterhub_ip)
+
+    yaml.dump(template, open(args.output_file, "w"), default_flow_style=False)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
