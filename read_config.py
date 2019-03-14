1a21d59e1c6f55420deb84c69e81ecfb02dc771a
Sarah Gibson <sgibson@turing.ac.uk>
2019-03-14 16:08:17 +0000

    Edit read_config.py to output all of config.json

--- read_config.py
+++ read_config.py
@@ -5,4 +5,7 @@ config = json.load(open(file, "r"))
 
 print(config["azure"]["subscription"], config["azure"]["res_grp_name"],
       config["azure"]["location"], config["azure"]["cluster_name"],
-      config["azure"]["node_count"], config["azure"]["vm_size"])
+      config["azure"]["node_count"], config["azure"]["vm_size"],
+      config["binderhub"]["name"], config["binderhub"]["version"],
+      config["docker"]["id"], config["docker"]["org"],
+      config["docker"]["image_prefix"], config["secretFile"])
