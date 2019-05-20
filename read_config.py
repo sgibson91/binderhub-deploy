1fd3f9279b14611391a7a00472efc65b1365921d
sgibson91 <44771837+sgibson91@users.noreply.github.com>
2019-05-20 15:47:54 +0100

    Remove docker id and secretFile from read_config.py

--- read_config.py
+++ read_config.py
@@ -7,5 +7,4 @@ print(config["azure"]["subscription"], config["azure"]["res_grp_name"],
       config["azure"]["location"], config["azure"]["cluster_name"],
       config["azure"]["node_count"], config["azure"]["vm_size"],
       config["binderhub"]["name"], config["binderhub"]["version"],
-      config["docker"]["id"], config["docker"]["org"],
-      config["docker"]["image_prefix"], config["secretFile"])
+      config["docker"]["org"], config["docker"]["image_prefix"])
