5dddd1b37f3a93198e35bc90455d09ce07c37f0d
sgibson91 <44771837+sgibson91@users.noreply.github.com>
2019-05-24 15:38:55 +0100

    Print pods in info.sh

--- info.sh
+++ info.sh
@@ -4,6 +4,11 @@
 configFile='config.json'
 binderhubname=`jq -r '.binderhub .name' ${configFile}`
 
+# Print pods
+echo "--> Printing pods"
+kubectl get pods -n $binderhubname
+echo
+
 # Get IP addresses of both the JupyterHub and BinderHub
 echo "Jupyterhub IP: " `kubectl --namespace=$binderhubname get svc proxy-public | awk '{ print $4}' | tail -n 1`
 echo "Binderhub IP: " `kubectl --namespace=$binderhubname get svc binder | awk '{ print $4}' | tail -n 1`
