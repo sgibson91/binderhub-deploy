8c836b805de4da984e3d797022b1c1fad95fe081
Tim Greaves <tim.greaves@imperial.ac.uk>
2019-05-24 11:40:55 +0100

    Fixing mac looping
    Big thanks to @sgibson91 for squashing this bug -- defeated me!
    
    Co-Authored-By: sgibson91 <44771837+sgibson91@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -190,7 +190,7 @@ az aks get-credentials -n $AKS_NAME -g $RESOURCE_GROUP_NAME -o table
 
 # Check nodes are ready
 nodecount="$(kubectl get node | awk '{print $2}' | grep Ready | wc -l)"
-while [[ ! x${nodecount} == x${AKS_NODE_COUNT} ]] ; do echo -n $(date) ; echo " : ${nodecount} of ${AKS_NODE_COUNT} nodes ready" ; sleep 15 ; nodecount="$(kubectl get node | awk '{print $2}' | grep Ready | wc -l)" ; done
+while [[ x${nodecount} -ne x${AKS_NODE_COUNT} ]] ; do echo -n $(date) ; echo " : ${nodecount} of ${AKS_NODE_COUNT} nodes ready" ; sleep 15 ; nodecount="$(kubectl get node | awk '{print $2}' | grep Ready | wc -l)" ; done
 echo
 echo "Cluster node status:"
 kubectl get node
