8f7d6e8fe66def2ca8f379c8037b50bfdde2dde8
Sarah Gibson <sgibson@turing.ac.uk>
2019-05-31 14:16:40 +0100

    Refactor info.sh

--- info.sh
+++ info.sh
@@ -2,13 +2,13 @@
 
 # Read in config.json and get variables
 configFile='config.json'
-binderhubname=`jq -r '.binderhub .name' ${configFile}`
+BINDERHUB_NAME=`jq -r '.binderhub .name' ${configFile}`
 
 # Print pods
 echo "--> Printing pods"
-kubectl get pods -n $binderhubname
+kubectl get pods -n ${BINDERHUB_NAME}
 echo
 
 # Get IP addresses of both the JupyterHub and BinderHub
-echo "Jupyterhub IP: " `kubectl --namespace=$binderhubname get svc proxy-public | awk '{ print $4}' | tail -n 1`
-echo "Binderhub IP: " `kubectl --namespace=$binderhubname get svc binder | awk '{ print $4}' | tail -n 1`
+echo "Jupyterhub IP: " `kubectl --namespace=${BINDERHUB_NAME} get svc proxy-public | awk '{ print $4}' | tail -n 1`
+echo "Binderhub IP: " `kubectl --namespace=${BINDERHUB_NAME} get svc binder | awk '{ print $4}' | tail -n 1`
