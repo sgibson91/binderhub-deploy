48e0481b822fb9dae0dac2ff6c38c9c6cf171f8a
Sarah Gibson <sgibson@turing.ac.uk>
2019-05-31 14:19:42 +0100

    Refactor upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,8 +3,8 @@
 # Read in config.json and get variables
 echo "--> Reading in config.json"
 configFile='config.json'
-binderhubname=`jq -r '.binderhub .name' ${configFile}`
-binderhubversion=`jq -r '.binderhub .version' ${configFile}`
+BINDERHUB_NAME=`jq -r '.binderhub .name' ${configFile}`
+HELM_BINDERHUB_VERSION=`jq -r '.binderhub .version' ${configFile}`
 
 # Pull and update helm chart repo
 echo "--> Updating helm chart repo"
@@ -12,9 +12,12 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
 helm repo update
 
 # Upgrade helm chart
-echo "--> Upgrading ${binderhubname}'s helm chart with version ${binderhubversion}"
-helm upgrade ${binderhubname} jupyterhub/binderhub --version=${binderhubversion} -f secret.yaml -f config.yaml
+echo "--> Upgrading ${BINDERHUB_NAME}'s helm chart with version ${HELM_BINDERHUB_VERSION}"
+helm upgrade ${BINDERHUB_NAME} jupyterhub/binderhub \
+--version=${HELM_BINDERHUB_VERSION} \
+-f secret.yaml \
+-f config.yaml
 
 # Print Kubernetes pods
 echo "--> Getting pods"
-kubectl get pods -n ${binderhubname}
+kubectl get pods -n ${BINDERHUB_NAME}
