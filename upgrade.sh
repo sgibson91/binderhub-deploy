09ccd35f7e982dbe3029900dddcb21b63bc71a34
Sarah Gibson <sgibson@turing.ac.uk>
2019-06-11 07:04:03 +0100

    Add script path to upgrade, logs, info scripts

--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# Get this script's path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Read in config.json and get variables
 echo "--> Reading in config.json"
-configFile='config.json'
+configFile="${DIR}/config.json"
 AKS_RESOURCE_GROUP=`jq -r '.azure .res_grp_name' ${configFile}`
 BINDERHUB_NAME=`jq -r '.binderhub .name' ${configFile}`
 BINDERHUB_VERSION=`jq -r '.binderhub .version' ${configFile}`
@@ -25,8 +28,8 @@ helm repo update
 echo "--> Upgrading ${BINDERHUB_NAME}'s helm chart with version ${BINDERHUB_VERSION}"
 helm upgrade $BINDERHUB_NAME jupyterhub/binderhub \
 --version=$BINDERHUB_VERSION \
--f ./secret.yaml \
--f ./config.yaml
+-f ${DIR}/secret.yaml \
+-f ${DIR}/config.yaml
 
 # Print Kubernetes pods
 echo "--> Getting pods"
