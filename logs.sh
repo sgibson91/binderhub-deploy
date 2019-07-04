7c7dc25db380403a5cf6ee1320080be8da3bd185
Sarah Gibson <44771837+sgibson91@users.noreply.github.com>
2019-07-04 09:16:54 +0100

    Reintroduce BINDERHUB_NAME var

--- logs.sh
+++ logs.sh
@@ -16,7 +16,7 @@ az aks get-credentials -n $AKS_NAME -g $AKS_RESOURCE_GROUP
 echo "--> Fetching JupyterHub logs"
 
 # Get pod name of the JupyterHub
-HUB_POD=`kubectl get pods -n hub23 -o=jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep "^hub-"`
+HUB_POD=`kubectl get pods -n ${BINDERHUB_NAME} -o=jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep "^hub-"`
 
 # Print the JupyterHub logs to the terminal
 kubectl logs ${HUB_POD} -n ${BINDERHUB_NAME}
