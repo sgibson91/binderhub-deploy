de846dc15ff1cab80c69b9b0166c178b06f46094
Sarah Gibson <sgibson@turing.ac.uk>
2019-09-08 10:04:53 +0200

    Correct bash syntax in upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,14 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Read in config.json and get variables
 echo "--> Reading in config.json"
 configFile="${DIR}/config.json"
-AKS_RESOURCE_GROUP=`jq -r '.azure .res_grp_name' ${configFile}`
-BINDERHUB_NAME=`jq -r '.binderhub .name' ${configFile}`
-BINDERHUB_VERSION=`jq -r '.binderhub .version' ${configFile}`
-AKS_NAME=`echo ${BINDERHUB_NAME}-AKS`
+AKS_RESOURCE_GROUP=$(jq -r '.azure .res_grp_name' "${configFile}")
+BINDERHUB_NAME=$(jq -r '.binderhub .name' "${configFile}")
+BINDERHUB_VERSION=$(jq -r '.binderhub .version' "${configFile}")
+AKS_NAME="${BINDERHUB_NAME}-AKS"
 
 # Get cluster credentials
 echo "--> Getting credentials for AKS cluster"
-az aks get-credentials -n $AKS_NAME -g $AKS_RESOURCE_GROUP
+az aks get-credentials -n "$AKS_NAME" -g "$AKS_RESOURCE_GROUP"
 
 # Initialise helm
 echo "--> Initialising helm"
@@ -26,11 +26,11 @@ helm repo update
 
 # Upgrade helm chart
 echo "--> Upgrading ${BINDERHUB_NAME}'s helm chart with version ${BINDERHUB_VERSION}"
-helm upgrade $BINDERHUB_NAME jupyterhub/binderhub \
---version=$BINDERHUB_VERSION \
--f ${DIR}/secret.yaml \
--f ${DIR}/config.yaml
+helm upgrade "$BINDERHUB_NAME" jupyterhub/binderhub \
+--version="$BINDERHUB_VERSION" \
+-f "${DIR}"/secret.yaml \
+-f "${DIR}"/config.yaml
 
 # Print Kubernetes pods
 echo "--> Getting pods"
-kubectl get pods -n $BINDERHUB_NAME
+kubectl get pods -n "$BINDERHUB_NAME"
