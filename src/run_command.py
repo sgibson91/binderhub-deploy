7f7a3dba351239330109b5de27c350c2017a6b80
Ubuntu <sgibson91@test-vm.3f1uvehzpmqergrgdyhc2ankmc.ax.internal.cloudapp.net>
2020-06-23 18:04:04 +0000

    Use try/except blocks in run_command.py

--- src/run_command.py
+++ src/run_command.py
@@ -15,12 +15,17 @@ def run_cmd(cmd):
     """
     result = {}
 
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = proc.communicate()
+    try:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = proc.communicate()
 
-    result["returncode"] = proc.returncode
-    result["output"] = output[0].decode(encoding="utf-8").strip("\n")
-    result["err_msg"] = output[1].decode(encoding="utf-8").strip("\n")
+        result["returncode"] = proc.returncode
+        result["output"] = output[0].decode(encoding="utf-8").strip("\n")
+        result["err_msg"] = output[1].decode(encoding="utf-8").strip("\n")
+    except FileNotFoundError:
+        result["returncode"] = 1
+        result["ouptut"] = ""
+        result["err_msg"] = "FileNotFoundError"
 
     return result
 
@@ -41,26 +46,32 @@ def run_pipe_cmd(cmds):
     procs = []  # List to track processes in
     result = {}  # Dictionary to store outputs in
 
-    for i in range(N):
-        if i == 0:
-            proc = subprocess.Popen(
-                cmds[i], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
-            procs.append(proc)
-        else:
-            proc = subprocess.Popen(
-                cmds[i],
-                stdin=procs[i - 1].stdout,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            procs[i - 1].stdout.close()
-            procs.append(proc)
+    try:
+        for i in range(N):
+            if i == 0:
+                proc = subprocess.Popen(
+                    cmds[i], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+                procs.append(proc)
+            else:
+                proc = subprocess.Popen(
+                    cmds[i],
+                    stdin=procs[i - 1].stdout,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+                procs[i - 1].stdout.close()
+                procs.append(proc)
 
-    output = procs[-1].communicate()
+        output = procs[-1].communicate()
 
-    result["returncode"] = procs[-1].returncode
-    result["output"] = output[0].decode(encoding="utf-8").strip("\n")
-    result["err_msg"] = output[1].decode(encoding="utf-8").strip("\n")
+        result["returncode"] = procs[-1].returncode
+        result["output"] = output[0].decode(encoding="utf-8").strip("\n")
+        result["err_msg"] = output[1].decode(encoding="utf-8").strip("\n")
+
+    except FileNotFoundError:
+        result["returncode"] = 1
+        result["output"] = ""
+        result["err_msg"] = "FileNotFoundError"
 
     return result
