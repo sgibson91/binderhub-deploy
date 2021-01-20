307f5df8d60183bce846db551e28561e257540f9
Sarah Gibson <sgibson@turing.ac.uk>
2021-01-20 12:55:39 +0000

    🚧 Clean up outputs

--- terraform/container_registry/outputs.tf
+++ terraform/container_registry/outputs.tf
@@ -1,7 +1,9 @@
 output "acr_id" {
-  value = azurerm_container_registry.acr.id
+  value      = azurerm_container_registry.acr.id
+  depends_on = [ azurerm_container_registry.acr ]
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  value      = azurerm_container_registry.acr.login_server
+  depends_on = [ azurerm_container_registry.acr ]
 }
