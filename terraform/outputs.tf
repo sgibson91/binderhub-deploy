a980e75e0656dbd40ea6dda1df3ac43ec834869e
Sarah Gibson <sgibson@turing.ac.uk>
2021-01-21 13:46:13 +0000

    ⚡️ Configure conditional outputs for ACR

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "acr_id" {
-  value      = module.container_registry[0].acr_id
+  value = length(module.container_registry) > 0 ? module.container_registry[0].acr_id : ""
 }
 
 output "acr_login_server" {
-  value      = module.container_registry[0].acr_login_server
+  value = length(module.container_registry) > 0 ? module.container_registry[0].acr_login_server : ""
 }
